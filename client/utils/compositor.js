652b873759de07154bf3c453c2e55e2fb4a1eafd
wuyuanqing <wuyuanqing527@gmail.com>
2019-03-02 15:55:23 +0800

    更新首页排序；

--- client/utils/compositor.js
+++ client/utils/compositor.js
@@ -12,9 +12,9 @@ var by = function (name, minor) {
         return typeof minor === 'function' ? minor(o, p) : 0;
       }
       if (typeof a === typeof b) {
-        return a < b ? -1 : 1;
+        return a > b ? -1 : 1;
       }
-      return typeof a < typeof b ? -1 : 1;
+      return typeof a > typeof b ? -1 : 1;
     } else {
       thro("error");
     }
